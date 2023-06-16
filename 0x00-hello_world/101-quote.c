64f4ca70d2c72d7c2f591d330861c679e02780df
7Mayo7 <mayar.mahmoud177@gmail.com>
2023-06-15 20:57:45 -0700

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,14 @@
 
 /**
 * main - Entry point
-* 
+*
 * Description: A C programme prints some string
-* 
+*
 * Return: Always 1 (Error)
-*/ 
+*/
 
 int main(void)
-{
-        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-        return (1);
+}
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }

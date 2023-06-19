e26ee91c8f26b15bedb1a996abbd54fb072013e7
7Mayo7 <mayar.mahmoud177@gmail.com>
2023-06-19 06:25:49 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,15 @@
 #include <unistd.h>
 
 /**
-* main - Entry point
-*
-* Description: print a qoute
-*
-* Return: Always 1 (not success)
+ * main - Entry point
+ *
+ * Description:quote programme prints some string
+ *
+ * Return: Always 1 (Error)
 */
 
 int main(void)
-{
-	char quo[] = "and that piece of art is useful\" -  Dora Korpar, 2015-10-19\";
+	{char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, quo, 59);
-	return (1);
-}
+return (1)

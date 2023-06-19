35f2fc0953f47096ca71b680a434c6da3c59e882
7Mayo7 <mayar.mahmoud177@gmail.com>
2023-06-19 06:31:57 -0700

    qoute

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,16 @@
 /**
  * main - Entry point
  *
- * Description:quote programme prints some string
+ * Description: print a qoute using write function size_t
+ * write (int fd, const void *buf . size_t count);
  *
- * Return: Always 1 (Error)
+ * Return: Always 1 (Error).
 */
 
 int main(void)
-	{char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, quo, 59);
-return (1)
+	return (1);
+}

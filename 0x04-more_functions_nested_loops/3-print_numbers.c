5216b39baf07a1c416276f0d87fb48cdd56c73d7
7Mayo7 <mayar.mahmoud177@gmail.com>
2023-06-22 17:00:29 -0700

    putchar

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * print_numbers - print 0 to 9
+ *only using _purchar twice
+ *Return: always 0 success
+ */
+
+void print_numbers(void)
+{
+	for (num >= 48 && num <= 57; num++)
+	_purchar(int);
+	return (0);
+}

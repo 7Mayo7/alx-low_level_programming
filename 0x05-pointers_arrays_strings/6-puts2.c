69e42c4e46cfc28eba72e1b2542de2c87a56f6e6
7Mayo7 <mayar.mahmoud177@gmail.com>
2023-06-28 15:20:18 -0700

    puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,17 +8,19 @@
  *@str:take the input from another
  * Return: void
  */
+
 void puts2(char *str)
 {
 	int a;
 
+
 	for (a = 0; str[a] != '\0'; ++a)
 	{
 	if (a % 2 == 0)
+
 	_putchar(str[a]);
 
+
 	}
-
 	_putchar('\n');
-
 }

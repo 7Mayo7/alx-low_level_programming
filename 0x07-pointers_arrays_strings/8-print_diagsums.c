c659fa66d704de1332597e15de44ebc872378c8a
7Mayo7 <mayar.mahmoud177@gmail.com>
2023-07-04 16:41:13 -0700

    string

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *set_string - sets the value of a pointer to a char
+ *@s: take the input
+ *@to: take the input
+ * Return: 0
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}

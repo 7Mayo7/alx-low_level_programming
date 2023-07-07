2c557951b649c188f4b148d6a890d2abd8c8345d
7Mayo7 <mayar.mahmoud177@gmail.com>
2023-07-07 15:00:07 -0700

    passed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,9 +2,9 @@
 
 /**
  *main - print number of arguments passed
- *@argc: number of commandline argument
- *@argv: pointer of array of commandline argument
- *Return: success 0 not zero fail
+ *@argc: number of commandline arguments
+ *@argv: pointer of array of commandline arguments
+ *Return: success 0, not zero fail
  */
 
 int main(int argc, char *argv[] _attribute_((unused)))

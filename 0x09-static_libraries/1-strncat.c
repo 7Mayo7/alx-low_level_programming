de6b4e14683f7598215f36f586b931086ac19cff
7Mayo7 <mayar.mahmoud177@gmail.com>
2023-07-07 16:53:51 -0700

    done

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,1 +1,15 @@
+#include "main.h"
+/**
+ *swap_int - swaps the values of two integers
+ *@a: take the input
+ *@b: take the input
+ * Return: void
+ */
 
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+
+	*a = *b;
+	*b = tmp;
+}

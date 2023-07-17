50db975c189b3c6656cc31f01588208c8ba62c65
7Mayo7 <mayar.mahmoud177@gmail.com>
2023-07-17 06:15:55 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - define a new type struct dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ * description : dog struct with Poppy
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

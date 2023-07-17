3ab5df085971f9de868624d4a33d9e6feab9fdaa
7Mayo7 <mayar.mahmoud177@gmail.com>
2023-07-17 07:14:39 -0700

    nw

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,19 +13,19 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-
+/* Allocate memory for the new dog_t structure */
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 	return (NULL);
 
-	Copy the name using strdup
+/*Copy the name using strdup*/
 	new_dog->name = strdup(name);
 	if (new_dog->name == NULL)
 	{
 	free(new_dog);
 	return (NULL);
 	}
-
+/* Copy the owner using strdup */
 	new_dog->owner = strdup(owner);
 	if (new_dog->owner == NULL)
 	{
@@ -34,7 +34,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	return (NULL);
 	}
 
-
+/* Assign the age */
 	new_dog->age = age;
 
 	return (new_dog);

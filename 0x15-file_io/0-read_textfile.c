f636b65f9261a5f30fce3374c5da025da14c19a5
7Mayo7 <mayar.mahmoud177@gmail.com>
2023-08-07 16:58:14 -0700

    read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,25 +1,42 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * read_textfile - Reads a text file
+ * prints it to the POSIX standard output.
  * @filename: The name of the file to read.
  * @letters: The number of letters to read and print.
  * Return: The actual number of letters read and printed, or 0 on failure.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t bytes;
-	char buf[READ_BUF_SIZE * 8];
+	ssize_t n_read, n_written;
+	char buffer[1024];
 
-	if (!filename || !letters)
-		return (0);
+	if (filename == NULL)
+	return (0);
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (0);
-	bytes = read(fd, &buf[0], letters);
-	bytes = writes(STDOUT_FILEND, &buf[0], bytes);
+	return (0);
+
+	n_read = read(fd, buffer, sizeof(buffer));
+	if (n_read == -1)
+	{
 	close(fd);
-	return (bytes);
+	return (0);
+	}
+
+	n_written = write(STDOUT_FILENO, buffer, n_read < letters ? n_read : letters);
+	if (n_written == -1 || n_written != n_read)
+	{
+	close(fd);
+	return (0);
+	}
+
+	close(fd);
+	return (n_written);
 }

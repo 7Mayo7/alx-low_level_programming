cf1dc55587dc8e8b55d01df98430548da74e4873
7Mayo7 <mayar.mahmoud177@gmail.com>
2023-08-07 17:23:36 -0700

    read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,32 +10,43 @@
  * @letters: The number of letters to read and print.
  * Return: The actual number of letters read and printed, or 0 on failure.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t fd, n_read, n_written;
-	char buffer[1024];
+	int fd, n_read, n_written;
+	char *buffer;
 
 	if (filename == NULL)
 	return (0);
-
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 	return (0);
+	buffer = malloc(sizeof(char) * letters);
 
-	n_read = read(fd, buffer, sizeof(buffer));
+	if (buffer == NULL)
+	{
+	close(fd);
+	return (0);
+	}
+	n_read = read(fd, buffer, letters);
+
 	if (n_read == -1)
 	{
+	free(buffer);
 	close(fd);
 	return (0);
 	}
 
-	n_written = write(STDOUT_FILENO, buffer, n_read < letters ? n_read : letters);
+	n_written = write(STDOUT_FILENO, buffer, n_read);
+
 	if (n_written == -1 || n_written != n_read)
 	{
+	free(buffer);
 	close(fd);
 	return (0);
 	}
-
+	free(buffer);
 	close(fd);
 	return (n_written);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <unistd.h>
 
 #define READ_BUF_SIZE 1024
 
@@ -14,5 +14,4 @@ int create_file(const char *filename, char *text_content);
 
 int append_text_to_file(const char *filename, char *text_content);
 
-int _putchar(char c);
 #endif

f59f1dd12704113e180315d94ef1656973dcf7f7
7Mayo7 <mayar.mahmoud177@gmail.com>
2023-10-26 14:21:22 -0700

    f

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,8 +12,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * 8))
 	return (-1);
-
-	*n &= ~(1UL << index);
+	*n &= ~(1 << index);
 	return (1);
 }
 
